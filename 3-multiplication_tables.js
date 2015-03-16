545c6dde4577d9d2cf750e2a85a55c6f7e808533
Tom Joseph <tom.joseph@detroitlabs.com>
2015-03-15 23:03:25 -0400

    using a string to collect numbers, output and rest the string on each row

--- 3-multiplication_tables.js
+++ 3-multiplication_tables.js
@@ -1,9 +1,11 @@
 function mTable(row, col) {
-	var string;
-	for (var x = 1; x < row; x++) {
-		for(var y = 1; y < col; y++) {
-			console.log((x*y) + " ");
+	var string = "";
+	for (var x = 1; x <= row; x++) {
+		for(var y = 1; y <= col; y++) {
+			string = string + (x*y) + " ";
 		}
+		console.log(string);
+		string = "";
 	}
 }
 
