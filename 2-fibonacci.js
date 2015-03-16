6508adadcb66980bb147b4259ab3e0aa13023f03
Tom Joseph <tom.joseph@detroitlabs.com>
2015-03-15 22:52:09 -0400

    adding summation of even terms; option to display results on each iteration

--- 2-fibonacci.js
+++ 2-fibonacci.js
@@ -1,16 +1,26 @@
-function fibonacci(n)
+function fibonacci(n, display)
 {
 	var last = 0;
 	var result = 1;
 	var temp;
 
-	for (var iteration = 1; iteration < n; iteration++){
-		console.log(result);
-		temp = result
-		result += last;
-		last = temp;
+	var evenSums = 0;
+
+	if(n < 4000000) {
+		for (var iteration = 1; iteration < n; iteration++){
+			if (display) { console.log(result); }
+
+			if (result % 2 === 0) { evenSums += result; }
+
+			temp = result
+			result += last;
+			last = temp;
+		}
 	}
+	return evenSums;
 }
 
 console.log("Standard fibonacci sequence: ");
-fibonacci(100);
+fibonacci(20, true);
+
+console.log("Sum of even results: ", fibonacci(20, false));
