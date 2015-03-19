19cce77b5db9c52127265cb586be2f40a867a6e2
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-03-18 20:38:06 -0400

    Setting up exercise

---
+++ harmonicsum.js
@@ -0,0 +1,6 @@
+/*Exercise 1: Harmonic Sum
+
+Write a program to compute the sum of a harmonic series, as shown below, where n=50000. Calculate the sum from left-to-right, and then from right-to-left.
+
+Harmonic(n) = 1 + 1/2 + 1/3 + 1/4 + .... + 1/n */
+
