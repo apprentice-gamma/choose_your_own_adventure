189905921263a2276ecafa7bf0675ae363de8520
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-03-18 23:48:26 -0400

    Submitting finished Harmonic Sum Exercise

--- harmonicsum.js
+++ harmonicsum.js
@@ -1,6 +1,16 @@
-/*Exercise 1: Harmonic Sum
+/*Write a program to compute the sum of a harmonic series, as shown below, where n=50000. Calculate the sum from left-to-right, and then from right-to-left.
 
-Write a program to compute the sum of a harmonic series, as shown below, where n=50000. Calculate the sum from left-to-right, and then from right-to-left.
+Harmonic(n) = 1 + 1/2 + 1/3 + 1/4 + .... + 1/n*/
 
-Harmonic(n) = 1 + 1/2 + 1/3 + 1/4 + .... + 1/n */
+var sget = require("sget");
+var harmony = [];
+var inputNum = sget("Please enter a number");
 
+for(var i=1; i<inputNum; i++){
+	harmony.push(1/i);
+}
+var leftToRight = eval(harmony.join('+'));
+var rightToLeft = eval(harmony.reverse().join('+'));
+
+console.log("Here is the harmonic sum of " + inputNum + " from left to right: " + leftToRight);
+console.log("Here is the harmonic sum of " + inputNum + " from right to left: " + rightToLeft);
